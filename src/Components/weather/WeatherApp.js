a13ac48a6b77242bbac06adf4e073e15e17f9199
Roshan Acharya <roshanacharya207@gmail.com>
2023-12-07 22:24:34 +0545

    new

--- src/Components/weather/WeatherApp.js
+++ src/Components/weather/WeatherApp.js
@@ -8,33 +8,37 @@ import "../weather/WeatherApp.css";
 
 export const WeatherApp = () => {
   return (
-    <div className="container">
-      <div className="top-bar">
-        <input type="text" className="Cityinput" placeholder="search" />
-        <div className="search-icon">
-          <img src={search_icon} alt="Search" />
-        </div>
-      </div>
-      <div>
-        <img src={cloud_icon} alt="" />
-      </div>
-      <div className="weather-temp">20º C</div>
-      <div className="weather-location">London</div>
-      <div className="data-container">
-        {/* element  */}
-        <div className="element">
-          <img src={humidity_icon} alt="" />
-          <div className="data">
-            <div className="humidity-percentage">64%</div>
-            <div className="text">Humidity</div>
+    <div>
+      <div class="progress-bar">Progressing......</div>
+
+      <div className="container">
+        <div className="top-bar">
+          <input type="text" className="Cityinput" placeholder="search" />
+          <div className="search-icon">
+            <img src={search_icon} alt="Search" />
           </div>
         </div>
-        {/* element  */}
-        <div className="element">
-          <img src={wind_icon} alt="" />
-          <div className="data">
-            <div className="humidity-percentage">18km/r</div>
-            <div className="text">Wind Speed</div>
+        <div>
+          <img src={cloud_icon} alt="" />
+        </div>
+        <div className="weather-temp">20º C</div>
+        <div className="weather-location">London</div>
+        <div className="data-container">
+          {/* element  */}
+          <div className="element">
+            <img src={humidity_icon} alt="" />
+            <div className="data">
+              <div className="humidity-percentage">64%</div>
+              <div className="text">Humidity</div>
+            </div>
+          </div>
+          {/* element  */}
+          <div className="element">
+            <img src={wind_icon} alt="" />
+            <div className="data">
+              <div className="humidity-percentage">18km/r</div>
+              <div className="text">Wind Speed</div>
+            </div>
           </div>
         </div>
       </div>
